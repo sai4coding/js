13e47eb8bc72bd68e8ca7e62f754245298751400
sai4coding <nagasaiisukapalli@gmail.com>
2024-10-22 13:12:10 +0530

    added function with arrays

--- practice.js
+++ practice.js
@@ -1,3 +1,15 @@
 function fun(){
-    document.getElementById('para').innerHTML="baguna";
+    document.getElementById('para').innerHTML="<h1>baguna</h1>";
 }
+
+function funn(){
+    document.getElementById('paraa').innerText="<h1>baguna</h1>";
+}
+
+function arrays(arr){
+    for(let i=0;i<arr.length;i++){
+        console.log(i);
+    }
+}
+let arr=[1,2,3]
+arrays(arr);
